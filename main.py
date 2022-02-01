409f6d519f9d3e2917bedc18d5c7477615c7c838
Akash Kethireddy <11202339@in.techwave.net>
2022-02-01 20:02:44 +0100

    Added print statement to main

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
     first_value = input("Enter first value: ")
     second_value = input("Enter second value: ")
     result = add(first_value,second_value)
+    print("Printing Result :")
     print(result)
 
